cb5025b0cd42037ed9e66358e7f88e87ccf2b1f4
Dame <dcampbell7095@gmail.com>
2020-11-14 20:44:43 -0500

    adding view route for notes.html file

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,5 @@
 const express = require ("express");
 
-require("./routes/html-routes");
-
-// require("./routes/api-routes");
-
 const app = express();
 
 const PORT = 8080;
@@ -11,6 +7,10 @@ const PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+require("./routes/html-routes")(app);
+
+// require("./routes/api-routes");
 
 app.listen(PORT, () =>{
     console.log(`App is listening on ${PORT}`)
