c864efd1d5673106d2ce75fdc974f42a7f436396
Dame <dcampbell7095@gmail.com>
2020-11-15 12:06:19 -0500

    saving the db.json path as a variable to be reused

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,18 +1,24 @@
 const path = require("path");
+const dbJson = "../db/db.json";
 
 // Need 3 Routes
 
 module.exports = function(app){
 
+
 // Route 1: get route for /api/notes endpoint
 
 app.get('/api/notes', (req, res) => {
 
-    res.sendFile(path.join(__dirname, "../db/db.json"))
+    res.sendFile(path.join(__dirname, dbJson))
     
  });
 
 // Route 2: post route for /api/notes
+
+// app.post("/api/notes", (req, res) => {
+
+// })
 
 // Route 3: delete route for /notes/:id
 
