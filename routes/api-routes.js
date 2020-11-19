b6bf51722e1c5c9b1ab294daadf542b431ffef74
Dame <dcampbell7095@gmail.com>
2020-11-19 18:41:17 -0500

    reading DB file and return the respons within delete route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
   // Route 1: get route for /api/notes endpoint
 
   app.get("/api/notes", (req, res) => {
-    res.json(db);
+    return res.json(db);
   });
 
   // Route 2: post route for /api/notes
@@ -32,20 +32,29 @@ module.exports = function (app) {
   // Route 3: delete route for /notes/:id
 
   app.delete("/api/notes/:id", (req, res) => {
+
+    let activeDB = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"))
+
     let activeNote = req.params.id;
 
     console.log(activeNote);
 
-    let result = db.filter(note => note.id != activeNote)
+    let result = activeDB.filter(note => note.id != activeNote)
 
     console.log(result);
-
-    
 
     fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(result), "utf8", (err, data) =>{
         if(err) throw err;
         console.log("new DB file created without deleted note.")
     })
+    return res.json(result)
 
   });
+
+  // app.delete("/api/notes/:id", (req, res) => {
+  //   userNote.findByIdAndRemove(req.params.id, function(err){
+  //     if(err){res.send(err);}
+  //     res.json({messaje:"Done"});
+  //   });
+  // })
 };
